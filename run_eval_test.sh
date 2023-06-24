7c698ce0319d305ca252b4858fc54188ec5f0cc8
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-06-24 08:40:01 +0700

    v

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
-	--module 'res' \
+	--module 'dst' \
 	--test_files '/kaggle/input/interim-data/interim/GradSearch/KETOD/test.json' \
   --batch_size 20 \
 	--num_beams   4 \
