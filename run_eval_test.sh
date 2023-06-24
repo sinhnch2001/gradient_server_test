2a645171657df8cb97b194e0470abdce2cf7a468
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-06-24 21:26:09 +0700

    v

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
 	--module 'res' \
-	--test_files '/kaggle/input/interim-data/interim/GradRes/KETOD/test.json' \
+	--test_files '/kaggle/input/interim-data/interim/GradRes/FUSHEDCHAT/test.json' \
   --batch_size 20 \
 	--num_beams   4 \
 	--with_tracking  \
 	--path_to_save_dir '/kaggle/input/epoch-29-res/epoch_29/pytorch_model.bin'\
-	--log_input_label_predict '/kaggle/working/log_input_label_predict.json'
+	--log_input_label_predict '/kaggle/working/log_input_label_predict_fc.json'
