faed69fc258bfc29ee77d07c4de3d9e75245782c
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-06-24 07:24:20 +0700

    v

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
-	--module 'dst' \
-	--test_files './data/interim/GradSearch/FUSEDCHAT/val.json' \
+	--module 'res' \
+	--test_files '/kaggle/input/interim-data/interim/GradRes/FUSHEDCHAT/test.json' \
   --batch_size 26 \
 	--num_beams   4 \
 	--with_tracking  \
-	--path_to_save_dir './output/GradSearch-08.06/epoch_44/pytorch_model.bin'
+	--path_to_save_dir '/kaggle/input/epoch-13-res/epoch_13/pytorch_model.bin'
