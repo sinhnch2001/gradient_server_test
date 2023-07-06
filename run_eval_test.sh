b99fd2b77465422c57bd943322c1f4cd37d1690d
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-07-06 13:44:19 +0700

    d

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
 	--module 'res' \
-	--test_files '/kaggle/working/interim/GradRes/ORQuAC/test.json' \
+	--test_files '/kaggle/working/interim/GradRes/WOW/test.json' \
   --batch_size 20 \
 	--num_beams 4 \
 	--with_tracking  \
 	--path_to_save_dir '/kaggle/input/epoch44-res-30-6/pytorch_model_epoch44_res_30-06.bin'\
-	--log_input_label_predict '/kaggle/working/orquac_res.json'
+	--log_input_label_predict '/kaggle/working/wow_res.json'
