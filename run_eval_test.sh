2efc48d1665b7d09806dc7216806c62e4f131511
sinhnch2001 <120398909+sinhnch2001@users.noreply.github.com>
2023-07-07 05:01:01 +0700

    d

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
-	--module 'dst_odd' \
-	--test_files '/kaggle/input/interim-data/interim_blv1/GradSearch/FUSEDCHAT/test_odd.json' \
+	--module 'dst_tod' \
+	--test_files '/kaggle/input/interim-data/interim_blv1/GradSearch/FUSEDCHAT/test_tod.json' \
   --batch_size 20 \
 	--num_beams 4 \
 	--with_tracking  \
 	--path_to_save_dir '/kaggle/input/epoch44-dst-6-7/module1_pytorch_model.bin'\
-	--log_input_label_predict '/kaggle/working/fusedchat_dst_odd.json'
+	--log_input_label_predict '/kaggle/working/fusedchat_dst_tod.json'
