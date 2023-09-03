ceba25a04638bc63927e995d4c2ca0be2659addf
This PC <sinhnch2001@gmail.com>
2023-09-03 21:49:59 +0700

    bla

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
 	--module 'dst_tod' \
 	--test_files '/kaggle/input/mw-extra/MW22/test_tod.json' \
-  --batch_size 20 \
+  --batch_size 16 \
 	--num_beams 4 \
 	--with_tracking  \
 	--path_to_save_dir '/kaggle/input/final-checkpoint-gradtod/model_dst.bin'\
