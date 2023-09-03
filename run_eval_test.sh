49bf318dbb38b9ab4b9eaf0fe650f1bc8338fcde
This PC <sinhnch2001@gmail.com>
2023-09-03 20:44:46 +0700

    bla

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
 	--module 'dst_tod' \
-	--test_files '/kaggle/input/interim-data/interim_blv1/GradSearch/FUSEDCHAT/test.json' \
+	--test_files '/kaggle/input/mw-extra/test_merge.json' \
   --batch_size 20 \
 	--num_beams 4 \
 	--with_tracking  \
-	--path_to_save_dir '/kaggle/input/epoch44-dst-6-7/module1_pytorch_model.bin'\
-	--log_input_label_predict '/kaggle/working/fusedchat_dst_tod.json'
+	--path_to_save_dir '/kaggle/input/final-checkpoint-gradtod/model_dst.bin'\
+	--log_input_label_predict '/kaggle/working/MW22.json'
