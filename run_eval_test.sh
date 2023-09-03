1e6519475a4c76b2c76ad463a7fb6afa795fd34a
This PC <sinhnch2001@gmail.com>
2023-09-03 21:07:02 +0700

    bla

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file src/config/config_fsdp.yaml src/models/eval_test.py 	\
 	--module 'dst_tod' \
-	--test_files '/kaggle/input/mw-extra/test_merge.json' \
+	--test_files '/kaggle/input/mw-extra' \
   --batch_size 20 \
 	--num_beams 4 \
 	--with_tracking  \
