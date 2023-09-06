7b38fd29aed26cf7092e83e33277bccc7013d071
This PC <sinhnch2001@gmail.com>
2023-09-06 18:47:05 +0700

    bla

--- src/features/convert_GradRes/WOW_Converter.py
+++ src/features/convert_GradRes/WOW_Converter.py
@@ -16,7 +16,7 @@ from typing import (
     Union,
     Optional
 )
-from  argparse import ArgumentParser
+from argparse import ArgumentParser
 from src.features.converter import DialConverter
 
 logger = logging.getLogger(__name__)
@@ -61,69 +61,69 @@ class WoW_Converter(DialConverter):
         logger.warning("No ontology is specified")
         return None
 
-    def define_instruct(self, 
+    def define_instruct(self,
                         instruct_file_path: str) -> List[str]:
-        with open(instruct_file_path,"r") as f:
+        with open(instruct_file_path, "r") as f:
             instructions = f.readlines()
         return instructions
 
     def get_instruction(self, list_instructions):
         random_instruction = list_instructions[random.randint(0, len(list_instructions) - 1)]
         return random_instruction[:-1]
-    
-    def get_context(self, 
-                    first_diag:str,
+
+    def get_context(self,
+                    first_diag: str,
                     list_turns: List[str]):
-            context = ""
-            """
-                Concat text with length for context
-                Ex: context = <tag_speaker> .... <tag_agent> .... <tag_speaker>
-            """
-            if first_diag is not None:    
-                context = context + "USER" + ": " + first_diag + " "
-            for turn in list_turns:
-                if turn["speaker"] == "1_Apprentice":
-                    context = context + self.tag_speaker + ": " + turn['text'] + " "
-                    continue
-                context = context + self.tag_agent + ": " + turn['text'] + " "
-            return context.strip()    
-    
+        context = ""
+        """
+            Concat text with length for context
+            Ex: context = <tag_speaker> .... <tag_agent> .... <tag_speaker>
+        """
+        if first_diag is not None:
+            context = context + "USER" + ": " + first_diag + " "
+        for turn in list_turns:
+            if turn["speaker"] == "1_Apprentice":
+                context = context + self.tag_speaker + ": " + turn['text'] + " "
+                continue
+            context = context + self.tag_agent + ": " + turn['text'] + " "
+        return context.strip()
+
     def get_knowledge(self, turn):
         if turn['checked_sentence'] == {}:
             return "no_passages_used"
         knowledge = list(turn['checked_sentence'].values())[0]
         return knowledge
 
-    def _process_diag(self, 
-                      sample: dict)-> List[dict]:
+    def _process_diag(self,
+                      sample: dict) -> List[dict]:
         processed_sample = []
         for i, turn in enumerate(sample['dialog']):
             info = {}
             # pass first iter
             if turn["speaker"] != "0_Wizard" or i == 0: continue
             if turn["speaker"] == "0_Wizard" and i <= self.window_context:
-                info["instruction"] = self.get_instruction(self.list_instructions) 
-                info['context']     = self.get_context(first_diag= sample['persona'], 
-                                                   list_turns= sample['dialog'][0:i])
-                info['documents']   = self.get_knowledge(turn)
-                info["style"]       = self.style_odd[np.random.randint(low=0, high=4)]
-                info['response']    = turn['text']
+                info["instruction"] = self.get_instruction(self.list_instructions)
+                info['context'] = self.get_context(first_diag=sample['persona'],
+                                                   list_turns=sample['dialog'][0:i])
+                info['documents'] = self.get_knowledge(turn)
+                # info["style"] = self.style_odd[np.random.randint(low=0, high=4)]
+                info['response'] = turn['text']
                 info['ontology'] = ''
                 info['system_action'] = ''
                 processed_sample.append(info)
                 continue
-            info["instruction"] = self.get_instruction(self.list_instructions) 
-            info['context']     = self.get_context(first_diag= None, 
-                                                   list_turns= sample['dialog'][(i-self.window_context):i])
-            info['documents']   = self.get_knowledge(turn)
-            info["style"]       = self.style_odd[np.random.randint(low=0, high=4)]
-            info['response']    = turn['text']
+            info["instruction"] = self.get_instruction(self.list_instructions)
+            info['context'] = self.get_context(first_diag=None,
+                                               list_turns=sample['dialog'][(i - self.window_context):i])
+            info['documents'] = self.get_knowledge(turn)
+            # info["style"] = self.style_odd[np.random.randint(low=0, high=4)]
+            info['response'] = turn['text']
 
             info['ontology'] = ''
             info['system_action'] = ''
             processed_sample.append(info)
         return processed_sample
-    
+
     def process(self, ratio_val: int, ratio_test: int) -> None:
         if ratio_test is not None and ratio_val is not None:
             self.process_with_ratio(ratio_val, ratio_test)
@@ -132,11 +132,11 @@ class WoW_Converter(DialConverter):
 
     def process_without_ratio(self) -> None:
         """
-        Convert raw train, val, test datasets to specified format 
+        Convert raw train, val, test datasets to specified format
         """
 
         logger.info("Loading ungrounded train, val, test datasets...")
-        train , val, test = self.load_datapath()
+        train, val, test = self.load_datapath()
 
         logger.info("Loading instructions...")
         self.list_instructions = self.define_instruct(self.instruct_file_path)
@@ -147,7 +147,7 @@ class WoW_Converter(DialConverter):
             processed_sample = self._process_diag(each_sample)
             self.train.extend(processed_sample)
 
-        self.val = []  
+        self.val = []
         for each_sample in val:
             processed_sample = self._process_diag(each_sample)
             self.val.extend(processed_sample)
@@ -161,17 +161,16 @@ class WoW_Converter(DialConverter):
         self.save_datapath()
         return None
 
-    
-    def process_with_ratio(self, 
-                valid_ratio: float,
-                test_ratio: float
-                ) -> None:
-
-        def distribute_datsets(
-                           dataset: List[Dict], 
+    def process_with_ratio(self,
                            valid_ratio: float,
                            test_ratio: float
-                           ) -> Tuple[List[Dict], List[Dict], List[Dict]]:
+                           ) -> None:
+
+        def distribute_datsets(
+                dataset: List[Dict],
+                valid_ratio: float,
+                test_ratio: float
+        ) -> Tuple[List[Dict], List[Dict], List[Dict]]:
             """Distribute data to train, val, test sets by type and ratio
             Args:
                 dataset (List[Dict]): dataset
@@ -194,7 +193,7 @@ class WoW_Converter(DialConverter):
             return train, valid, test
 
         logger.info("Loading file ungrounded dataset...")
-        with open(os.path.join(self.file_path, "data.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "data.json"), mode="r", encoding="utf-8") as f:
             self.data = json.load(f)
 
         logger.info("Loading instructions...")
@@ -205,15 +204,14 @@ class WoW_Converter(DialConverter):
         for each_sample in self.data:
             samples = self._process_diag(each_sample)
             processed_data.extend(samples)
-        
-        self.train , self.val, self.test = distribute_datsets(dataset= processed_data, 
-                                                              valid_ratio = valid_ratio, 
-                                                              test_ratio = test_ratio)
+
+        self.train, self.val, self.test = distribute_datsets(dataset=processed_data,
+                                                             valid_ratio=valid_ratio,
+                                                             test_ratio=test_ratio)
 
         logger.info("Saving grounded train, val, test datasets...")
         self.save_datapath()
         return None
-
 
     def save_datapath(self) -> None:
         """
@@ -226,10 +224,9 @@ class WoW_Converter(DialConverter):
             json.dump(self.val, f, indent=4)
 
         with open(os.path.join(self.save_path, "test.json"), "w+") as f:
-           json.dump(self.test, f, indent=4)
+            json.dump(self.test, f, indent=4)
         return None
-        
-    
+
     def load_datapath(self) -> Tuple[List[dict], List[dict]]:
         """
         Load dataset from two path files.
@@ -237,19 +234,19 @@ class WoW_Converter(DialConverter):
             tuple[list[dict], list[dict]]: dialogues and facts of each dialogues
         """
 
-        with open(os.path.join(self.file_path, "train.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "train.json"), mode="r", encoding="utf-8") as f:
             train = json.load(f)
 
-        with open(os.path.join(self.file_path, "valid_random_split.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "valid_random_split.json"), mode="r", encoding="utf-8") as f:
             val_random_dts = json.load(f)
 
-        with open(os.path.join(self.file_path, "valid_topic_split.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "valid_topic_split.json"), mode="r", encoding="utf-8") as f:
             val_topic_dts = json.load(f)
 
-        with open(os.path.join(self.file_path, "test_random_split.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "test_random_split.json"), mode="r", encoding="utf-8") as f:
             test_random_dts = json.load(f)
 
-        with open(os.path.join(self.file_path, "test_topic_split.json"), mode= "r", encoding="utf-8") as f:
+        with open(os.path.join(self.file_path, "test_topic_split.json"), mode="r", encoding="utf-8") as f:
             test_topic_dts = json.load(f)
 
         return train, val_random_dts + val_topic_dts, test_random_dts + test_topic_dts
@@ -309,21 +306,22 @@ def parse_args() -> ArgumentParser:
         raise ValueError(
             "You are running script without input data. "
             "Make sure you set all input data to --dataset_file_path")
-    
-    if args.ratio_val is not None and args.ratio_test is None:  
+
+    if args.ratio_val is not None and args.ratio_test is None:
         raise ValueError(
             "You are running script with specified ratio"
             "Make sure you set both input to --ratio_val and --ratio_test")
-    elif args.ratio_test is None and args.ratio_test is not None:  
+    elif args.ratio_test is None and args.ratio_test is not None:
         raise ValueError(
             "You are running script with specified ratio"
             "Make sure you set both input to --ratio_val and --ratio_test")
-    
+
     if args.ratio_val is not None and args.ratio_test is not None:
         logger.warning("""You are running with specified ratio for validation dataset and test dataset..."
                         Converter will automatically get the total raw data - `data.json` file - which is available in the extracted data folder""")
 
     return args
+
 
 def config_logger(log_file) -> None:
     """ Config logger
@@ -337,26 +335,25 @@ def config_logger(log_file) -> None:
         format="[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s",
         handlers=handlers
     )
-    
+
 
 def make_dataset():
-
     args = parse_args()
     config_logger(args.log_file)
 
     dataset_file_path = os.path.join(os.getcwd(), args.dataset_file_path)
     save_file_path = os.path.join(os.getcwd(), args.save_path)
-    
+
     wow_obj = WoW_Converter(
-                        file_path = dataset_file_path,
-                        save_path= save_file_path,
-                        window_context= args.window_context,
-                        instruct_file_path= args.instruct_path
-                    )
-    wow_obj.process(ratio_val= args.ratio_val,
+        file_path=dataset_file_path,
+        save_path=save_file_path,
+        window_context=args.window_context,
+        instruct_file_path=args.instruct_path
+    )
+    wow_obj.process(ratio_val=args.ratio_val,
                     ratio_test=args.ratio_test)
     logger.info("Done")
-    
+
 
 if __name__ == "__main__":
     make_dataset()
