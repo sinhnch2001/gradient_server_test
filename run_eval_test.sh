2d5173193f1dac5d5108da6ac993cf751c479ef4
This PC <sinhnch2001@gmail.com>
2023-09-15 21:50:37 +0700

    bla

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file /kaggle/working/gradient_server_test/src/config/config_fsdp.yaml /kaggle/working/gradient_server_test/src/models/test_evaluation.py 	\
 	--module 'dst_tod' \
-	--model_name 'google/flan-t5-small' \
+	--model_name 'google/flan-t5-base' \
 	--test_files '/kaggle/input/interim-data/GradSearch/GradSearch_v1.1/FUSEDCHAT/test.json' \
   --batch_size 18 \
 	--num_beams 4 \
