4f4724f05732f0226f81cd175c293b3dc17f0901
This PC <sinhnch2001@gmail.com>
2023-09-18 22:56:40 +0700

    bla

--- src/data/dataloader_GradSearch.py
+++ src/data/dataloader_GradSearch.py
@@ -115,14 +115,12 @@ class StateDataLoader:
 
         inp_tokens = self.tokenizer.batch_encode_plus(
             inputs,
-            max_length=True,
             padding=True,
             return_tensors="pt",
             truncation=True,
         )
         tgt_tokens = self.tokenizer.batch_encode_plus(
             targets,
-            max_length=True,
             padding=True,
             return_tensors="pt",
             truncation=True,
