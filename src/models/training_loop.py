6e5a815b0bb9fb204949502f69bbea94c363d86b
This PC <sinhnch2001@gmail.com>
2023-09-19 09:05:13 +0700

    bla

--- src/models/training_loop.py
+++ src/models/training_loop.py
@@ -292,7 +292,6 @@ class Trainer:
             if self.with_tracking:
                 total_loss = 0
             for step, batch in enumerate(dataloaders['train']):
-                print(len(batch["input_ids"][0]))
                 # We need to skip steps until we reach the resumed step
                 if self.resume_from_checkpoint and epoch == starting_epoch:
                     if resume_step is not None and step < resume_step:
