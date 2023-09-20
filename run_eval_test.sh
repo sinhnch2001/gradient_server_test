b61d4a82dde49f4b1001a650de8755cf9fbbbf85
This PC <sinhnch2001@gmail.com>
2023-09-20 17:55:18 +0700

    bla

--- run_eval_test.sh
+++ run_eval_test.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file /kaggle/working/gradient_server_test/src/config/config_fsdp_t5.yaml /kaggle/working/gradient_server_test/src/models/test_evaluation.py 	\
 	--module 'dst_tod' \
 	--model_name 'google/flan-t5-base' \
-	--test_files '/kaggle/input/gradsearch-v2-pro/GradSearch_v2/FUSEDCHAT/test.json' \
+	--test_files '/kaggle/input/gradsearch-v2-pro/GradSearch_v2/SGD/test.json' \
   --batch_size 8 \
 	--with_tracking  \
 	--path_to_save_dir '/kaggle/input/final-checkpoint-gradtod/pytorch_model_epoch_2.bin'\
-	--log_input_label_predict '/kaggle/working/FUSEDCHAT.json'
+	--log_input_label_predict '/kaggle/working/SGD.json'
